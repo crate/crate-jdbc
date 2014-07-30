82f0b54bf58f01a67dbbedd46f1332a54d708b29
Christian Haudum <christian.haudum@gmail.com>
2014-07-30 15:09:20 +0200

    prepare release 1.0.4

--- src/main/java/io/crate/client/jdbc/CrateDriverVersion.java
+++ src/main/java/io/crate/client/jdbc/CrateDriverVersion.java
@@ -27,7 +27,7 @@ import org.elasticsearch.monitor.jvm.JvmInfo;
 public class CrateDriverVersion {
 
     public static final boolean SNAPSHOT = false;
-    public static final CrateDriverVersion CURRENT = new CrateDriverVersion(10003, SNAPSHOT);
+    public static final CrateDriverVersion CURRENT = new CrateDriverVersion(10004, SNAPSHOT);
 
     public final int id;
     public final byte major;
