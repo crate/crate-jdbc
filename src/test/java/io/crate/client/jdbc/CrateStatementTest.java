4d28afbb348314c39f87563780c4b70bad16d2fc
Matthias Wahl <matthias@crate.io>
2014-08-13 16:47:24 +0200

    refactored unit tests

--- src/test/java/io/crate/client/jdbc/CrateStatementTest.java
+++ src/test/java/io/crate/client/jdbc/CrateStatementTest.java
@@ -24,65 +24,26 @@ package io.crate.client.jdbc;
 
 import io.crate.action.sql.SQLRequest;
 import io.crate.action.sql.SQLResponse;
-import io.crate.client.CrateClient;
 import io.crate.types.*;
-import org.elasticsearch.action.ActionFuture;
-import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.common.collect.MapBuilder;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashSet;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-public class CrateStatementTest {
+public class CrateStatementTest extends AbstractCrateJDBCTest {
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
-
-    private Connection connection;
-
-    @Before
-    public void prepare() throws Exception {
-        CrateClient crateClient = mock(CrateClient.class);
-        Answer<ActionFuture<SQLResponse>> sqlAnswer = new Answer<ActionFuture<SQLResponse>>() {
-            @Override
-            public ActionFuture<SQLResponse> answer(InvocationOnMock invocation) throws Throwable {
-                assert invocation.getArguments().length == 1;
-                return fakeExecuteSQL(invocation.getArguments()[0]);
-            }
-        };
-        when(crateClient.sql((SQLRequest)any())).thenAnswer(sqlAnswer);
-        when(crateClient.sql(anyString())).thenAnswer(sqlAnswer);
-
-        connection = new CrateConnection(crateClient, "crate://localhost:4300");
-    }
-
-    private ActionFuture<SQLResponse> fakeExecuteSQL(Object o) {
-
-        final SQLResponse response;
-
-        if ((o instanceof String && ((String) o).startsWith("SELECT")) ||
-                (o instanceof SQLRequest && ((SQLRequest) o).stmt().toUpperCase().startsWith("SELECT"))) {
+    @Override
+    protected SQLResponse getResponse(SQLRequest request) {
+        SQLResponse response;
+        if (request.stmt().toUpperCase().startsWith("SELECT")) {
             response = new SQLResponse(
                     new String[]{"boo", "i", "l", "f", "d", "s", "t", "o", "al", "ss", "n"},
                     new Object[][]{
@@ -113,18 +74,7 @@ public class CrateStatementTest {
         } else {
             response = new SQLResponse(new String[0], new Object[0][], 4L, System.currentTimeMillis());
         }
-
-        return new PlainActionFuture<SQLResponse>() {
-            @Override
-            public SQLResponse get() throws InterruptedException, ExecutionException {
-                return response;
-            }
-
-            @Override
-            public SQLResponse get(long timeout, TimeUnit unit) throws InterruptedException, TimeoutException, ExecutionException {
-                return response;
-            }
-        };
+        return response;
     }
 
     @Test
