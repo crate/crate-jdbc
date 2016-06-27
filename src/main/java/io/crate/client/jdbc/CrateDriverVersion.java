6d7e20e588a97650eb8a181728be57f55c89db5a
Christian Haudum <christian.haudum@gmail.com>
2016-06-27 11:03:14 +0200

    prepare release 1.12.3

--- src/main/java/io/crate/client/jdbc/CrateDriverVersion.java
+++ src/main/java/io/crate/client/jdbc/CrateDriverVersion.java
@@ -26,8 +26,8 @@ import io.crate.shade.org.elasticsearch.monitor.jvm.JvmInfo;
 
 public class CrateDriverVersion {
 
-    public static final boolean SNAPSHOT = true;
-    public static final CrateDriverVersion CURRENT = new CrateDriverVersion(11300, SNAPSHOT);
+    public static final boolean SNAPSHOT = false;
+    public static final CrateDriverVersion CURRENT = new CrateDriverVersion(11203, SNAPSHOT);
 
     public final int id;
     public final byte major;
