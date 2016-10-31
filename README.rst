e8c0a9f8da489f85bba0698e9a6a24acdf7cf17e
kovrus <ruslan@crate.io>
2016-10-31 15:18:13 +0100

    refactor tests and update readme/docs

--- README.rst
+++ README.rst
@@ -49,22 +49,6 @@ or::
 Afterwards a JAR file of the current version exists under ``build/lib``.
 
 
-Version 2.x
-===========
-
-Version 2 of the Crate JDBC driver is using the `PostgreSQL Wire Protocol`_ and
-is currently only intended for testing purposes.
-It is only available via `Jitpack`_ and won't be available on `Bintray`_ until
-Crate version ``0.57.x`` is released.
-
-Differences to version 1.x are:
-
-- The `connection string`_ changed to the same format as PostgreSQL JDBC,
-  which means that setting the default schema with the connection string is not
-  supported. Use ``setDefaultSchema()`` instead.
-- Support for the experimental ``showsubcolumns`` parameter has been dropped.
-
-
 Documentation
 =============
 
@@ -107,7 +91,5 @@ software solely pursuant to the terms of the relevant commercial agreement.
 
 
 .. _Crate: https://github.com/crate/crate
-.. _`PostgreSQL Wire Protocol`: https://www.postgresql.org/docs/current/static/protocol.html
 .. _Bintray: https://bintray.com/crate/crate/crate-jdbc
 .. _Jitpack: https://jitpack.io/#crate/crate-jdbc
-.. _`connection string`: https://jdbc.postgresql.org/documentation/80/connect.html
--- driver/test/java/io/crate/client/jdbc/integrationtests/CrateJDBCIntegrationTest.java
+++ driver/test/java/io/crate/client/jdbc/integrationtests/CrateJDBCIntegrationTest.java
@@ -25,14 +25,20 @@ package io.crate.client.jdbc.integrationtests;
 import com.carrotsearch.randomizedtesting.RandomizedTest;
 import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
 import io.crate.testing.CrateTestCluster;
+import io.crate.testing.CrateTestServer;
+import org.junit.AfterClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
 @ThreadLeakScope(ThreadLeakScope.Scope.SUITE)
 public class CrateJDBCIntegrationTest extends RandomizedTest {
 
-    private static final String CRATE_SERVER_VERSION = "0.56.2";
+    private static final String CRATE_SERVER_VERSION = "0.57.0";
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -42,4 +48,91 @@ public class CrateJDBCIntegrationTest extends RandomizedTest {
         .fromVersion(CRATE_SERVER_VERSION)
         .keepWorkingDir(false)
         .build();
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        tearDownTables();
+    }
+
+    static String getConnectionString() {
+        CrateTestServer server = testCluster.randomServer();
+        return String.format("crate://%s:%s/", server.crateHost(), server.psqlPort());
+    }
+
+    private static void tearDownTables() throws SQLException {
+        try (Connection conn = DriverManager.getConnection(getConnectionString())) {
+            ResultSet rs = conn.createStatement()
+                .executeQuery("select schema_name, table_name " +
+                              "from information_schema.tables where schema_name " +
+                              "not in ('pg_catalog', 'sys', 'information_schema', 'blob')");
+            while (rs.next()) {
+                conn.createStatement().execute(String.format(
+                    "drop table if exists \"%s\".\"%s\"", rs.getString("schema_name"), rs.getString("table_name")
+                ));
+            }
+        }
+    }
+
+    static void setUpTestTable() throws SQLException, InterruptedException {
+        try (Connection conn = DriverManager.getConnection(getConnectionString())) {
+            conn.createStatement().execute(
+                "create table if not exists test (" +
+                " id integer primary key," +
+                " string_field string," +
+                " boolean_field boolean," +
+                " byte_field byte," +
+                " short_field short," +
+                " integer_field integer," +
+                " long_field long," +
+                " float_field float," +
+                " double_field double," +
+                " timestamp_field timestamp," +
+                " object_field object as (\"inner\" string)," +
+                " ip_field ip" +
+                ") clustered by (id) into 1 shards with (number_of_replicas=0)");
+        }
+        ensureYellow();
+    }
+
+    static void insertIntoTestTable() throws SQLException {
+        Map<String, Object> objectField = new HashMap<String, Object>() {{
+            put("inner", "Zoon");
+        }};
+        try (Connection conn = DriverManager.getConnection(getConnectionString())) {
+            PreparedStatement preparedStatement =
+                conn.prepareStatement("insert into test (id, string_field, boolean_field, byte_field, " +
+                                      "short_field, integer_field, long_field, float_field, double_field, object_field, " +
+                                      "timestamp_field, ip_field) values " +
+                                      "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(2, "Youri");
+            preparedStatement.setBoolean(3, true);
+            preparedStatement.setByte(4, (byte) 120);
+            preparedStatement.setShort(5, (short) 1000);
+            preparedStatement.setInt(6, 1200000);
+            preparedStatement.setLong(7, 120000000000L);
+            preparedStatement.setFloat(8, 1.4f);
+            preparedStatement.setDouble(9, 3.456789);
+            preparedStatement.setObject(10, objectField);
+            preparedStatement.setTimestamp(11, new Timestamp(1000L));
+            preparedStatement.setString(12, "127.0.0.1");
+            preparedStatement.execute();
+            conn.createStatement().execute("refresh table test");
+        }
+    }
+
+    static void ensureYellow() throws SQLException, InterruptedException {
+        while (countUnassigned() > 0) {
+            Thread.sleep(100);
+        }
+    }
+
+    private static Long countUnassigned() throws SQLException {
+        try (Connection conn = DriverManager.getConnection(getConnectionString())) {
+            ResultSet rs = conn.createStatement()
+                .executeQuery("SELECT count(*) FROM sys.shards WHERE state != 'STARTED'");
+            rs.next();
+            return rs.getLong(1);
+        }
+    }
 }
