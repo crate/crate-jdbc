83e14a9cf25861193232d68dc734e1cad21b1705
Naomi Slater <nslater@tumbolia.org>
2017-07-10 19:21:34 +0200

    fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,6 @@
 from crate.theme.rtd.conf.crate_jdbc import *
+
+# crate.theme sets html_favicon to favicon.png which causes a warning because
+# it should be a .ico and in addition there is no favicon.png in this project
+# so it can't find the file
+html_favicon = None
