330da44aa9d8140e978eb6ccfb08bceb378a09e7
joemoe <johannes@crate.io>
2018-02-03 12:17:25 +0100

    enabled sitemap generation through sphinx module in the docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
 from crate.theme.rtd.conf.crate_jdbc import *
 
 exclude_patterns = ['eggs/**', 'requirements.txt']
+site_url = 'https://crate.io/docs/clients/jdbc/en/latest/'
+extensions = ['sphinx_sitemap']
