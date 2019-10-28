9a22f6a8939d8c85b971ec0e24aa7bd9b15dede7
Naomi Slater <nslater@tumbolia.org>
2019-10-28 13:38:06 +0100

    Update config module name

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,5 @@
-from crate.theme.rtd.conf.crate_jdbc import *
+from crate.theme.rtd.conf.clients_jdbc import *
+
+exclude_patterns = ['eggs/**', 'requirements.txt']
+site_url = 'https://crate.io/docs/clients/jdbc/en/latest/'
+extensions = ['sphinx_sitemap']
