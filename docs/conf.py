fc64f7280e82b9cc101f994016eecb6c8697d465
Naomi Slater <nslater@tumbolia.org>
2019-10-30 18:03:23 +0100

    Fixup cherry-pick

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,1 @@
 from crate.theme.rtd.conf.clients_jdbc import *
-
-exclude_patterns = ['eggs/**', 'requirements.txt']
-site_url = 'https://crate.io/docs/clients/jdbc/en/latest/'
-extensions = ['sphinx_sitemap']
