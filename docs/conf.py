949d25565a86dd5c6cae309551558b7e34900732
Andreas Motl <andreas.motl@crate.io>
2024-10-08 23:34:17 +0200

    Documentation: Update Sphinx conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,12 @@
 from crate.theme.rtd.conf.jdbc import *
+
+exclude_patterns = ['.crate-docs/**', 'requirements.txt']
+
+linkcheck_anchors_ignore = [
+    r"diff-.*",
+]
+
+# Enable version chooser.
+html_context.update({
+    "display_version": True,
+})
