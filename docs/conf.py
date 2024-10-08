03929ae7dcd3a06d5f60ae75586db9f9854d78f6
Andreas Motl <andreas.motl@crate.io>
2024-10-08 23:31:19 +0200

    Documentation: Enable version chooser

--- docs/conf.py
+++ docs/conf.py
@@ -5,3 +5,8 @@ exclude_patterns = ['.crate-docs/**', 'requirements.txt']
 linkcheck_anchors_ignore = [
     r"diff-.*",
 ]
+
+# Enable version chooser.
+html_context.update({
+    "display_version": True,
+})
